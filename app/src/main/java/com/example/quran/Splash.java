977b9c01af26733727ba6d1cd122b5d3d7f2aae9
MuhammadSaqibShoaib <muhammadsaqib54@outlook.com>
2022-12-26 01:44:40 +0500

    Splash Activity activiated

--- app/src/main/java/com/example/quran/Splash.java
+++ app/src/main/java/com/example/quran/Splash.java
@@ -2,14 +2,24 @@ package com.example.quran;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class Splash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         getSupportActionBar().hide();
+
+        Intent intent = new Intent(this, List.class);
+        startActivity(intent);
+
+
+
     }
+
+
 }
