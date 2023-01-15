d7712a4edea94057ef5d9cd0c44ab070f712b832
MuhammadSaqibShoaib <muhammadsaqib54@outlook.com>
2023-01-15 17:15:29 +0500

    Search button updated
    - In this commit search button functionality added

--- app/src/main/java/com/example/quran/MainActivity.java
+++ app/src/main/java/com/example/quran/MainActivity.java
@@ -4,18 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     String[] data;
     ListView listView;
+
+    EditText search_txt;
+    Button searchBtn;
+
     VerseAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        search_txt = findViewById(R.id.num_txt);
+        searchBtn = findViewById(R.id.search_btn);
+
+
         // getting data from previous activity
         Intent intent = getIntent();
         data = intent.getStringArrayExtra("data");
@@ -25,7 +39,24 @@ public class MainActivity extends AppCompatActivity {
 
         adapter = new VerseAdapter(getApplicationContext(),data);
         listView.setAdapter(adapter);
-        listView.smoothScrollToPosition(5);
+
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Log.d("Loaded", "Search button working");
+                String txt = search_txt.getText().toString();
+                Log.d("Loaded", "String text: "+txt);
+                int position = Integer.parseInt(txt);
+                Log.d("Loaded", "Position: "+position);
+                if (position > 0 && position < data.length)
+                    listView.smoothScrollToPosition(position);
+                else{
+                    Toast.makeText(getApplicationContext(),R.string.verse_error,Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
 
     }
 }
